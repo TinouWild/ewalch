ce95e73483388cf508082a4b1d5d947ab44be087
Tinou <tinouclt@gmail.com>
2023-11-29 21:49:39 +0100

    mobile design

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
-scp -rp /app/public/ ubuntu@217.182.71.207:/stacks/ewalch/storage/app/
+scp -rp ./app/public/ ubuntu@217.182.71.207:/stacks/ewalch/storage/app/
+scp ewalch_latest.tar.gz ubuntu@217.182.71.207:/stacks/ewalch/
