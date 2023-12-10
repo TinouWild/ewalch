3ebfd16eade701793d999fb36991a9b7f9275a1c
Tinou <tinouclt@gmail.com>
2023-12-10 18:10:27 +0100

    fix display social icons on desktop

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 docker build -t ewalch-prod_php:latest .
 docker save ewalch-prod_php:latest | gzip > ewalch_latest.tar.gz
-scp ewalch_latest.tar.gz ubuntu@217.182.71.207:/ewalch/ewalch_latest.tar.gz
